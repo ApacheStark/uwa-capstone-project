323644de4461b6540742bcdb7d7cd4b40110dfd4
Sam Holt <samgholt20@gmail.com>
2022-09-29 19:24:08 +1000

    init of admission rate prep and capstone constants

---
+++ capstone_constants.py
@@ -0,0 +1,8 @@
+data_path = '../../DATA/'
+
+data_paths = {
+   'triage': data_path + 'triage.csv',
+    'edstays': data_path + 'edstays.csv
+    }
+
+test = 'hi'
--- capstone_functions.py
+++ capstone_functions.py
@@ -1,8 +1,8 @@
 def get_admission(x):
-    if x['disposition'] == 'HOME':
-        return 0
-    else:
+    if x['disposition'] in ['ADMITTED', 'TRANSFER']:
         return 1
+    else:
+        return 0
         
 def get_expired(x):
     if x['disposition'] == 'EXPIRED':
@@ -20,3 +20,52 @@ def look_n_load(path, describe=False):
     print('\nMissing proportions:\n', df.isna().sum()/len(df))
     print('\nData:\n', df.head())
     return df
+
+
+
+def work_hours(x):
+    if x >= 8 and x <= 18:
+        return 1 
+    else:
+        return 0
+    
+    
+def length_of_stay(x, binary=False):
+    if binary:
+        print('Binary infers a likelhood of leaving within the next 24 hours')
+        if x == 0:
+            return 1
+        else: 
+            return 0
+    else: 
+        print('Going with ordered caterogical, ensure to encode using pandas.Categorical(ordered=True)')
+        if x == 0:
+            return 'Within the Day'
+        elif x <= 7:
+            return 'Within the Week'
+        elif x <= 28:
+            return 'Within the Month'
+        else:
+            return 'Beyond a Month'
+        
+        
+def rescale_pain(x):
+    try:
+        x = int(x)
+        if x > 100:
+            return 10
+        elif x > 10:
+            return x/10
+        elif x < 0:
+            return 0
+        else:
+            return x
+    except:
+        return None
+ 
+# assumes temp is accidentally scaled 10x when x > 200
+def rescale_temp(x):
+    if x > 200:
+        return x/10
+    else:
+        return x
